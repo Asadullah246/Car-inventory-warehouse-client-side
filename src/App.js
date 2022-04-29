b40ded5c4d7d7f321a5fd9a66a0d52955a3b3b45
Asadullah246 <asadm2258@gmail.com>
2022-04-29 10:02:39 +0600

    firs-time-testing

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>this is starting and testing</h2>
+     <Routes>
+       <Route></Route>
+     </Routes>
+    </div>
+  );
+}
+
+export default App;
