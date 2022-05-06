c7ca5ba70e4e61b79cf48aad3658a483caadd5c4
Asadullah246 <asadm2258@gmail.com>
2022-05-07 02:56:24 +0600

    env included

--- src/Firebase.init.js
+++ src/Firebase.init.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 import {getAuth} from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9C-os7poEqqYaSgXmXxld09yQj2n9ATw",
-  authDomain: "car-inventory-7c113.firebaseapp.com",
-  projectId: "car-inventory-7c113",
-  storageBucket: "car-inventory-7c113.appspot.com",
-  messagingSenderId: "463644814441",
-  appId: "1:463644814441:web:4edb480d8a5d54977946f8",
-  measurementId: "G-4M2RQE7PRM"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain ,
+  projectId:process.env.REACT_APP_projectId ,
+  storageBucket:process.env.REACT_APP_storageBucket ,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId ,
+  appId:process.env.REACT_APP_appId ,
+  measurementId:process.env.REACT_APP_measurementId 
 };
 
 // Initialize Firebase
